4e0e6edbd8d7896043a82bd023591463d69d0a77
Susikumar-Pitchaimuthu <susikumar.pitchaimuthu@fssa.freshworks.com>
2023-05-16 19:06:14 +0530

    chore: invite response details showing function added

--- assets/js/invite_card.js
+++ assets/js/invite_card.js
@@ -78,3 +78,264 @@ let inviteGlimpse = (document.getElementById("inviteGlimpse").value =
   findInvite["inviteGlimpse"]);
 let inviteExpand = (document.getElementById("inviteExplanation").value =
   findInvite["inviteExplanation"]);
+
+//  invite like details showing function
+
+let detailsDiv = document.querySelector(
+  ".invite-details-showing-div-container"
+);
+
+let responseHeadPara = document.querySelector(".response-head-para");
+
+let memberPara = document.querySelector(".member-para");
+
+let heartDiv = document.querySelector(".chart-div");
+
+let registerData = JSON.parse(localStorage.getItem("register"));
+
+heartDiv.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (detailsDiv.style.display === "none") {
+    detailsDiv.style.display = "block";
+  } else {
+    detailsDiv.style.display = "none";
+  }
+
+  // remove previous responce
+
+  let responseCardElement = document.querySelectorAll(".responce-card");
+
+  if (responseCardElement !== null) {
+    for (let resCard of responseCardElement) {
+      resCard.remove();
+    }
+  }
+
+  let heartResponce = findInvite["inviteHeart"];
+  console.log(heartResponce);
+  console.log(heartResponce.length);
+
+  //  setting the respective head for the response
+
+  responseHeadPara.innerHTML = "Like response details";
+
+  memberPara.innerHTML = heartResponce.length + " Members";
+
+  // Getting the heart response datas
+
+  let heartData = [];
+
+  for (let heart of heartResponce) {
+    for (let regisPerson of registerData) {
+      if (regisPerson["userId"] == heart) {
+        heartData.push(regisPerson);
+      }
+    }
+  }
+
+  console.log(heartData);
+
+  for (let heartDetail of heartData) {
+    let responseCard = document.createElement("div");
+    responseCard.setAttribute("class", "responce-card");
+
+    let insideResponseCardDiv = document.createElement("div");
+    insideResponseCardDiv.setAttribute("class", "inside-response-card-div");
+    responseCard.append(insideResponseCardDiv);
+
+    let responseImageDiv = document.createElement("div");
+    responseImageDiv.setAttribute("class", "response-image-div");
+    insideResponseCardDiv.append(responseImageDiv);
+
+    let responseImage = document.createElement("img");
+    responseImage.setAttribute("class", "response-image");
+    responseImage.setAttribute("alt", "user-image");
+    responseImage.setAttribute("src", heartDetail["avatarUrl"]);
+    responseImageDiv.append(responseImage);
+
+    let responsePersonAboutDiv = document.createElement("div");
+    responsePersonAboutDiv.setAttribute("class", "response-person-about-div");
+    insideResponseCardDiv.append(responsePersonAboutDiv);
+
+    let nameH3 = document.createElement("h3");
+    nameH3.setAttribute("class", "name-h3");
+    nameH3.innerHTML = heartDetail["userName"];
+    responsePersonAboutDiv.append(nameH3);
+
+    let personTheme = document.createElement("p");
+    personTheme.setAttribute("class", "person-theme");
+    personTheme.innerHTML = heartDetail["userTheme"];
+    responsePersonAboutDiv.append(personTheme);
+
+    document.querySelector(".invite-inside-response-div").append(responseCard);
+  }
+});
+
+// Invite dislike details showing function
+
+let likeDiv = document.querySelector(".chart-div-three");
+
+likeDiv.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (detailsDiv.style.display === "none") {
+    detailsDiv.style.display = "block";
+  } else {
+    detailsDiv.style.display = "none";
+  }
+
+  // remove previous responce
+  let responseCardElement = document.querySelectorAll(".responce-card");
+
+  if (responseCardElement !== null) {
+    for (let resCard of responseCardElement) {
+      resCard.remove();
+    }
+  }
+
+  let okResponse = findInvite["inviteLike"];
+
+  //  setting the respective head for the response
+
+  responseHeadPara.innerHTML = "Ok response details";
+
+  memberPara.innerHTML = okResponse.length + " Members";
+
+  let okData = [];
+
+  for (let ok of okResponse) {
+    for (let regisPerson of registerData) {
+      if (regisPerson["userId"] == ok) {
+        heartData.push(regisPerson);
+      }
+    }
+  }
+
+  // creting element of yes persons
+
+  for (let okPerson of okData) {
+    let responseCard = document.createElement("div");
+    responseCard.setAttribute("class", "responce-card");
+
+    let insideResponseCardDiv = document.createElement("div");
+    insideResponseCardDiv.setAttribute("class", "inside-response-card-div");
+    responseCard.append(insideResponseCardDiv);
+
+    let responseImageDiv = document.createElement("div");
+    responseImageDiv.setAttribute("class", "response-image-div");
+    insideResponseCardDiv.append(responseImageDiv);
+
+    let responseImage = document.createElement("img");
+    responseImage.setAttribute("class", "response-image");
+    responseImage.setAttribute("alt", "user-image");
+    responseImage.setAttribute("src", okPerson["avatarUrl"]);
+    responseImageDiv.append(responseImage);
+
+    let responsePersonAboutDiv = document.createElement("div");
+    responsePersonAboutDiv.setAttribute("class", "response-person-about-div");
+    insideResponseCardDiv.append(responsePersonAboutDiv);
+
+    let nameH3 = document.createElement("h3");
+    nameH3.setAttribute("class", "name-h3");
+    nameH3.innerHTML = okPerson["userName"];
+    responsePersonAboutDiv.append(nameH3);
+
+    let personTheme = document.createElement("p");
+    personTheme.setAttribute("class", "person-theme");
+    personTheme.innerHTML = okPerson["userTheme"];
+    responsePersonAboutDiv.append(personTheme);
+
+    document.querySelector(".invite-inside-response-div").append(responseCard);
+  }
+});
+
+// Invite dislike details showing function
+
+let dislikeDiv = document.querySelector(".chart-div-two");
+
+dislikeDiv.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (detailsDiv.style.display === "none") {
+    detailsDiv.style.display = "block";
+  } else {
+    detailsDiv.style.display = "none";
+  }
+
+  // remove previous responce
+
+  let responseCardElement = document.querySelectorAll(".responce-card");
+
+  if (responseCardElement !== null) {
+    for (let resCard of responseCardElement) {
+      resCard.remove();
+    }
+  }
+
+  let noResponce = findInvite["inviteNo"];
+
+  //  setting the respective head for the response
+
+  responseHeadPara.innerHTML = "No response details";
+
+  memberPara.innerHTML = noResponce.length + " Members";
+
+  let noData = [];
+
+  for (let no of noResponce) {
+    for (let regisPerson of registerData) {
+      if (regisPerson["userId"] == no) {
+        noData.push(regisPerson);
+      }
+    }
+  }
+
+  // No person element creation
+
+  for (let noDetails of noData) {
+    let responseCard = document.createElement("div");
+    responseCard.setAttribute("class", "responce-card");
+
+    let insideResponseCardDiv = document.createElement("div");
+    insideResponseCardDiv.setAttribute("class", "inside-response-card-div");
+    responseCard.append(insideResponseCardDiv);
+
+    let responseImageDiv = document.createElement("div");
+    responseImageDiv.setAttribute("class", "response-image-div");
+    insideResponseCardDiv.append(responseImageDiv);
+
+    let responseImage = document.createElement("img");
+    responseImage.setAttribute("class", "response-image");
+    responseImage.setAttribute("alt", "user-image");
+    responseImage.setAttribute("src", noDetails["avatarUrl"]);
+    responseImageDiv.append(responseImage);
+
+    let responsePersonAboutDiv = document.createElement("div");
+    responsePersonAboutDiv.setAttribute("class", "response-person-about-div");
+    insideResponseCardDiv.append(responsePersonAboutDiv);
+
+    let nameH3 = document.createElement("h3");
+    nameH3.setAttribute("class", "name-h3");
+    nameH3.innerHTML = noDetails["userName"];
+    responsePersonAboutDiv.append(nameH3);
+
+    let personTheme = document.createElement("p");
+    personTheme.setAttribute("class", "person-theme");
+    personTheme.innerHTML = noDetails["userTheme"];
+    responsePersonAboutDiv.append(personTheme);
+
+    document.querySelector(".invite-inside-response-div").append(responseCard);
+  }
+});
+
+// remove details card function
+
+let removeDiv = document.querySelector(".remove-div");
+console.log(removeDiv);
+removeDiv.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (detailsDiv.style.display === "block") {
+    detailsDiv.style.display = "none";
+  }
+});
